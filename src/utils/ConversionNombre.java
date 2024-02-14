24f96e6558f2a984492ff32ec0862bb1b199efcd
Mickaël Abin <abin.mickael@gmail.com>
2024-02-14 13:17:50 +0100

    java POO methodes statiques

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ConversionNombre {
+    public static void main(String[] args) {
+        String nombreEntier = "475";
+
+        int nombre = Integer.parseInt(nombreEntier);
+        System.out.println(nombre);
+        int a = 5;
+        int b = 78;
+        int c = Math.max(a,b);
+        System.out.println(c);
+    }
+}
